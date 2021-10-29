e32a19c30a48cda9ee574fa926e62fe4f33ead19
bhautik koladiya <bhautik211298@gmail.com>
2021-10-29 10:50:47 +0530

    api changes

--- src/app/product/router.js
+++ src/app/product/router.js
@@ -5,7 +5,7 @@ const controller = require( "./controller" );
 
 const router = express.Router( );
 
-router.get("/get", controller.getApplication)
+router.post("/get", controller.getApplication)
 router.post("/create", controller.createApplication);
 router.put("/edit/:id", controller.editApplication);
 router.delete("/delete/:id", controller.deleteApplication)
