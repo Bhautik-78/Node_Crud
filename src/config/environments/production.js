32db7b39966a33dcf2d418e47e54f7389577af41
bhautik koladiya <bhautik211298@gmail.com>
2021-11-10 10:12:02 +0530

    create mongo url

--- src/config/environments/production.js
+++ src/config/environments/production.js
@@ -1,7 +1,8 @@
 module.exports = {
     host: "localhost",
     port: 9999,
-    mongoUrl: "mongodb://localhost:27017/NodeCrud",
+    // mongoUrl: "mongodb://localhost:27017/NodeCrud",
+    mongoUrl: "mongodb+srv://admin:admin@cluster0.cvnf2.mongodb.net/NodeCrud?retryWrites=true&w=majority",
     logLevel: "debug",
     secret: "superSuperSecret",
 };
