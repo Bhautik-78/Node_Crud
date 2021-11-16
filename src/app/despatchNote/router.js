9c033fc16b8e635d3608cae976b4e133697e4b6c
bhautik koladiya <bhautik211298@gmail.com>
2021-11-16 14:57:04 +0530

    get data by Id in desPatch

--- src/app/despatchNote/router.js
+++ src/app/despatchNote/router.js
@@ -7,6 +7,7 @@ const router = express.Router( );
 
 router.post("/create", controller.createApplication);
 router.get("/get", controller.getApplication)
+router.get("/get/:id", controller.getApplicationForID)
 router.put("/edit/:id", controller.editApplication);
 router.delete("/delete/:id", controller.deleteApplication)
 
