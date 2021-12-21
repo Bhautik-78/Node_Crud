816524f1581cf08df135596571ec0cb4f5aaf2cd
bhautik koladiya <bhautik211298@gmail.com>
2021-12-21 16:11:21 +0530

    invoice excel upload and download

--- src/app/invoice/controller.js
+++ src/app/invoice/controller.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const User = require("../auth/model");
+const excel = require("exceljs");
+const XLSX = require("xlsx");
+const moment = require("moment")
 const Invoice = mongoose.model("invoice");
 require('dotenv').config();
 
@@ -86,6 +89,143 @@ exports.deleteApplication = async (req, res) => {
             res.status(400).send({message: "something Went Wrong"})
         }
     } catch (err) {
+        res.status(500).send({message: err.message || "data does not exist"});
+    }
+};
+
+exports.downloadExcel = async (req, res) => {
+    try {
+        let workbook = new excel.Workbook();
+        let worksheet = workbook.addWorksheet("sample");
+        worksheet.columns = [
+            {header: "PONumber", key: "PONumber", width: 20},
+            {header: "PODate", key: "PODate", width: 20},
+            {header: "invoiceNumber", key: "invoiceNumber", width: 20},
+            {header: "invoiceDate", key: "invoiceDate", width: 20},
+            {header: "NoOfPackages", key: "NoOfPackages", width: 20},
+            {header: "netWeight", key: "netWeight", width: 20},
+            {header: "grossWeight", key: "grossWeight", width: 20},
+            {header: "invoiceValue", key: "invoiceValue", width: 20},
+            {header: "CGSTValue", key: "CGSTValue", width: 20},
+            {header: "SGSTValue", key: "SGSTValue", width: 20},
+            {header: "IGSTValue", key: "IGSTValue", width: 20},
+            {header: "paymentReceived", key: "paymentReceived", width: 20},
+        ];
+        res.setHeader(
+            "Content-Type",
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        );
+        res.setHeader(
+            "Content-Disposition",
+            "attachment; filename=" + "sampleInvoiceSheet.xlsx"
+        );
+
+        return workbook.xlsx.write(res).then(function () {
+            res.status(200).end();
+        });
+    } catch (err) {
+        res.status(500).send({message: err.message || "data does not exist"});
+    }
+};
+
+const processExcel = workbook => {
+    const sheetNamesList = workbook.SheetNames;
+    const filesData = {};
+    sheetNamesList.forEach(sheetName => {
+        const worksheet = workbook.Sheets[sheetName];
+        const headers = {};
+        const data = [];
+        for (const z in worksheet) {
+            if (z[0] === "!") continue;
+            let tt = 0;
+            for (let i = 0; i < z.length; i++) {
+                if (!isNaN(z[i])) {
+                    tt = i;
+                    break
+                }
+            }
+            const col = z.substring(0, tt)
+            const row = parseInt(z.substring(tt))
+            const value = worksheet[z].v
+            if (row == 1 && value) {
+                headers[col] = value;
+                continue
+            }
+            if (!data[row]) data[row] = {};
+            data[row][headers[col]] = value
+        }
+        data.shift();
+        data.shift();
+        filesData[sheetName] = data
+    })
+    return filesData
+}
+
+exports.uploadExcel = async (req, res) => {
+    try {
+        const promiseBuilder = {
+            updateAppPromise: (payload, userID) => new Promise(async (resolve) => {
+                payload.PODate = moment(payload.PODate).format("YYYY-MM-DD")
+                payload.invoiceDate = moment(payload.invoiceDate).format("YYYY-MM-DD")
+                payload.userID = userID;
+                const isCreated = await Invoice.create(payload);
+                if (isCreated && isCreated._id) {
+                    return resolve({success: true})
+                } else {
+                    return resolve({success: false})
+                }
+            })
+        };
+        const {userID} = req.body;
+        const {file} = req;
+        const workbook = XLSX.readFile(`./uploads/${file.originalname}`, {
+            cellDates: true
+        })
+        let payload = [];
+        const allPromises = [];
+        const data = processExcel(workbook);
+        Object.keys(data).forEach(v => {
+            const filter = data[v]
+            payload.push(...filter)
+        });
+
+        const obj = await Invoice.find({});
+
+        const invoiceList = obj.map(item => item.invoiceNumber);
+
+        const removePayload = [];
+        payload.forEach((item, index) => (
+            (invoiceList.includes(item.invoiceNumber) ? removePayload.push(index + 1) : "")
+        ))
+        payload = payload.filter(item => !invoiceList.includes(item.invoiceNumber))
+
+        if (!removePayload.length) {
+            const filteredArr = payload.reduce((acc, current) => {
+                const x = acc.find(item => item.invoiceNumber === current.invoiceNumber);
+                if (!x) {
+                    return acc.concat([current]);
+                } else {
+                    return acc;
+                }
+            }, []);
+            if (filteredArr && filteredArr.length > 0) {
+                filteredArr.forEach(item => {
+                    allPromises.push(promiseBuilder.updateAppPromise(item, userID))
+                });
+                await Promise.all(allPromises).then(values => {
+                    if (values.some(value => value.success)) {
+                        res.status(200).send({success: true, message: "Successfully created"})
+                    } else {
+                        res.status(400).send({success: false, message: "There are not records are found!"})
+                    }
+                })
+            } else {
+                res.status(400).send({success: false, message: "No Data Found"})
+            }
+        }else {
+            res.status(201).send({status: false, message: `Row no. ${removePayload.toString()} Invoice Number duplicated`})
+        }
+    }catch (err) {
         res.status(500).send({message: err.message || "data does not exist"});
     }
 };
--- src/app/invoice/router.js
+++ src/app/invoice/router.js
@@ -1,5 +1,6 @@
 require( "./model" );
 const express = require( "express" );
+const multer =require("multer");
 
 const controller = require( "./controller" );
 
@@ -7,10 +8,23 @@ const validateToken = require("../../middlewares/validateToken")
 
 const router = express.Router();
 
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+        cb(null, "./uploads")
+    },
+    filename (req, file, cb) {
+        cb(null , file.originalname)
+    }
+});
+
+const upload = multer({ storage });
+
 router.post("/create",validateToken,  controller.createApplication);
 router.get("/get",validateToken,  controller.getApplication)
 router.get("/get/:id",validateToken, controller.getApplicationForID)
 router.put("/edit/:id",validateToken, controller.editApplication);
 router.delete("/delete/:id",validateToken, controller.deleteApplication)
+router.get("/download", controller.downloadExcel)
+router.post("/excelUpload", upload.single("file"), controller.uploadExcel)
 
 module.exports = router;
