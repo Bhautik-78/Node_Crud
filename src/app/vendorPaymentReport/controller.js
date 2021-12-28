0f54749dbcd20c56d966bdcecaddf12e79f9df0a
bhautik koladiya <bhautik211298@gmail.com>
2021-12-28 15:15:47 +0530

    changes

--- src/app/vendorPaymentReport/controller.js
+++ src/app/vendorPaymentReport/controller.js
@@ -8,34 +8,23 @@ require('dotenv').config();
 exports.createPaymentReport = async (req, res) => {
     try {
         const {authorization = ''} = req.headers;
-        const UserDetail = await User.findOne({accessToken : authorization});
-        if(UserDetail){
+        const UserDetail = await User.findOne({accessToken: authorization});
+        if (UserDetail) {
             req.body.paymentDate = moment(new Date()).format('YYYY-MM-DD');
             req.body.userId = UserDetail._id;
             const invoiceData = await Invoice.findOne({invoiceNumber: req.body.invoiceNumber});
-            const applicationData = await PaymentReport.find({invoiceNumber : req.body.invoiceNumber});
-            if(applicationData.length){
-                let total = 0;
-                applicationData.forEach(item => {
-                    total += item.amount;
-                });
-                const value =  Number(req.body.amount) + total;
-                const finalValue = invoiceData.invoiceValue - (value || 0);
-                await Invoice.updateOne({invoiceNumber: req.body.invoiceNumber},{invoiceValue : finalValue});
-            }else {
-                const finalValue = invoiceData.invoiceValue - (Number(req.body.amount) || 0);
-                await Invoice.updateOne({invoiceNumber: req.body.invoiceNumber},{invoiceValue : finalValue});
-            }
+            const finalValue = invoiceData.invoiceValue - (Number(req.body.amount) || 0);
+            await Invoice.updateOne({invoiceNumber: req.body.invoiceNumber}, {invoiceValue: finalValue});
             const isCreated = await PaymentReport.create(req.body);
-            if(isCreated){
+            if (isCreated) {
                 res.status(200).send({message: "successFully created"})
-            }else {
+            } else {
                 res.status(400).send({message: "something Went Wrong"})
             }
-        }else {
+        } else {
             res.status(400).send({message: "something went wrong"})
         }
-    }catch (err) {
+    } catch (err) {
         res.status(500).send({message: err.message || "data does not exist"});
     }
 };
